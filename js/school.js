ff1a59bc76588aa09015e52d0b1cfb4b4c061d86
Yhorm <aguilar_andres@protonmail.com>
2018-11-23 18:34:35 -0500

    declaratively rendered education data to the DOM using Vue js's template syntax.

---
+++ js/school.js
@@ -0,0 +1,8 @@
+var school = new Vue({
+  el: '#school',
+  data: {
+    name: "freeCodeCamp",
+    cert: "Responsive Web Design Certificate",
+    url: "https://www.freecodecamp.org/certification/aguilarandres/responsive-web-design"
+  }
+})
