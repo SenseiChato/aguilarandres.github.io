3e049c8edfdfd3d8207b71e49554d360a8ae828c
Yhorm <aguilar_andres@protonmail.com>
2018-11-22 19:45:41 -0500

    implemented vue js list rendering with 'v-for' to display technologies and languages.

---
+++ js/languages.js
@@ -0,0 +1,9 @@
+var lang = new Vue({
+  el: '#lang',
+  data: {
+    items: [
+      { language: 'English' },
+      { language: 'Spanish' }
+    ]
+  }
+})
---
+++ js/programming.js
@@ -0,0 +1,14 @@
+var tech = new Vue({
+  el: '#tech',
+  data: {
+    items: [
+      { message: 'c++' },
+      { message: 'python' },
+      { message: 'javascript' },
+      { message: 'vue js' },
+      { message: 'html' },
+      { message: 'css' },
+      { message: 'git' }
+    ]
+  }
+})
