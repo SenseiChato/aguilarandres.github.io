897caf09370a29f80a324e01ba8109b9517e1383
Yhorm <aguilar_andres@protonmail.com>
2018-12-28 16:36:03 -0800

    changed id

--- js/school.js
+++ js/school.js
@@ -1,10 +1,10 @@
-var school = new Vue({
-  el: '#school',
+var freeCodeCamp = new Vue({
+  el: '#freeCodeCamp',
   data: {
     name: "freeCodeCamp",
     cert: "Responsive Web Design Certificate",
     url: "https://www.freecodecamp.org/certification/aguilarandres/responsive-web-design",
     month: 'NOV',
-    year: '2018'        
+    year: '2018'
   }
 })
