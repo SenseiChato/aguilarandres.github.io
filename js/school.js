a9f8f8dbe511ca392d60b29b07c6b98d25f6f9dd
Yhorm <aguilar_andres@protonmail.com>
2018-12-28 16:44:00 -0800

    added school.

--- js/school.js
+++ js/school.js
@@ -8,3 +8,14 @@ var freeCodeCamp = new Vue({
     year: '2018'
   }
 })
+
+var gatech = new Vue({
+  el: '#gatech',
+  data: {
+    name: "Georgia Institute of Technology",
+    cert: "Computing in Python: Objects and Algorithms",
+    url: "https://courses.edx.org/certificates/2b2140d3771e4d8b87acae69f0ec5f35",
+    month: 'SEP',
+    year: '2018'
+  }
+})
